5968748c2ee8ac7880644d7b7af268aea9c0755a
Jon holder <jonathan.holder1@digital.homeoffice.gov.uk>
2026-04-21 16:08:06 +0100

    Added config + deployment change to fix deployment issues so the pvc is released before a new pod spins up

--- server.js
+++ server.js
@@ -13,6 +13,16 @@ settings = Object.assign({}, settings, {
   routes: settings.routes.map(require)
 });
 
+process.on('SIGTERM', () => {
+  console.log('SIGTERM received, shutting down gracefully');
+  process.exit(0);
+});
+
+process.on('SIGINT', () => {
+  console.log('SIGINT received, shutting down gracefully');
+  process.exit(0);
+});
+
 
 // overwrite bank holiday json once a day
 setInterval(() => {
